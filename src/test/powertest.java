9650da4317b27bddb78173e7c238ed8a4959e6da
Somesh-Pillai <57011503+Somesh-Pillai@users.noreply.github.com>
2022-01-11 21:43:17 +0530

    naming-conventions

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -1,26 +1,29 @@
-package powerpackage;
-
-
 import org.junit.Test;
+import org.junit.Before;
 
-public class powertest {
+public class PowerTest {
+    private PowerFinder powerFinder;
+    @Before
+    public void setUp () {
+        this.powerFinder = new PowerFinder();
+    }   
     @Test
-    public void one_raised_to_one_is_onew() {
-        assert powerfinder.OF(1,1) == 1;
+    public void oneRaisedtoOneisOne() {
+        assert powerFinder.of(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+    public void twoRaisedToOneisTwo() {
+        assert powerFinder.of(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
-        assert powerfinder.OF(2, 2) == 4;
+    public void twoRaisedToTwoisFour() {
+        assert powerFinder.of(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+    public void ThreeRaisedToTwoisNine() {
+        assert powerFinder.of(3, 2) == 9;
     }
 }
